aca3c0ec3241badebc6ab2732b4d0948aa6b5982
briz1 <my1queen8my1king@gmail.com>
2024-01-10 20:44:17 +0700

    First task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("Ф.И.О. сотрудника: " + fullName);
+
     }
 }
